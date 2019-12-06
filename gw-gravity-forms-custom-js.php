69c0a7dd601e51c5e1e0aec8cdeea069583cf078
Ruzgfpegk <Ruzgfpegk@zero-sum.fr>
2019-12-06 16:16:34 +0900

    Adjusting the code a bit more to the WordPress coding standard.
    Also made sure the output JS has the correct relative indentation.
    
    * https://make.wordpress.org/core/handbook/best-practices/coding-standards/php/
    * https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/

--- gw-gravity-forms-custom-js.php
+++ gw-gravity-forms-custom-js.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Gravity Wiz // Gravity Forms // Custom Javascript
  *
  * Include custom Javascript with your form.
  *
- * @version  1.31
+ * @version  1.32
  * @author   David Smith <david@gravitywiz.com>
  * @author   Ruzgfpegk <Ruzgfpegk@zero-sum.fr>
  * @license  GPL-2.0+
@@ -14,7 +15,7 @@
  * Plugin URI:   https://github.com/Ruzgfpegk/gw-gravity-forms-custom-js
  * Description:  Include custom Javascript with your form.
  * Author:       Gravity Wiz and contributors
- * Version:      1.31
+ * Version:      1.32
  * Author URI:   https://gravitywiz.com
  *
  * Usage:
@@ -26,62 +27,60 @@
  * Whenever this feed is set as the active feed, the submit button label will automatically be set to your custom label.
  */
 class GF_Custom_JS {
-
-	private static $instance = null;
-
+	
+	private static $instance;
+	
 	private $scripts = array();
-
+	
 	public static function get_instance() {
-		if ( self::$instance === null ) {
+		if ( null === self::$instance ) {
 			self::$instance = new self;
 		}
+		
 		return self::$instance;
 	}
-
+	
 	private function __construct() {
-
 		add_action( 'init', array( $this, 'init' ) );
-
 	}
-
+	
 	public function init() {
-
-		if( ! class_exists( 'GFForms' ) ) {
+		if ( ! class_exists( 'GFForms' ) ) {
 			return;
 		}
-
+		
 		if ( current_user_can( 'administrator' ) ) {
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_editor_script' ) );
-			add_filter( 'gform_form_settings', array( $this, 'add_custom_js_setting' ), 10, 2 );
+			add_action( 'admin_enqueue_scripts',        array( $this, 'enqueue_editor_script'  ) );
+			add_filter( 'gform_form_settings',          array( $this, 'add_custom_js_setting'  ), 10, 2 );
 			add_filter( 'gform_pre_form_settings_save', array( $this, 'save_custom_js_setting' ), 10, 2 );
 		}
-
+		
 		add_filter( 'gform_pre_render', array( $this, 'load_form_script' ), 10, 2 );
-
 	}
-
+	
 	public function enqueue_editor_script() {
-
-		if ( GFForms::get_page() !== 'form_settings' ) {
+		
+		if ( 'form_settings' !== GFForms::get_page() ) {
 			return;
 		}
-
+		
 		$editor_settings['codeEditor'] = wp_enqueue_code_editor( array( 'type' => 'text/javascript' ) );
+		
 		wp_localize_script( 'jquery', 'editor_settings', $editor_settings );
-
+		
 		wp_enqueue_script( 'wp-theme-plugin-editor' );
+		
 		wp_enqueue_style( 'wp-codemirror' );
-
 	}
-
+	
 	public function add_custom_js_setting( $settings, $form ) {
-
+		
 		$settings[ __( 'Custom Javascript' ) ] = array(
 			'custom_js' => sprintf(
 				'<tr id="custom_js_setting" class="child_setting_row">
 					<td colspan="2">
 						<p style="margin-top:-1rem;">%s</p>
-						<textarea id="custom_js" name="custom_js" spellcheck="false" 
+						<textarea id="custom_js" name="custom_js" spellcheck="false"
 							style="width:100%%;height:14rem;">%s</textarea>
 					</td>
 				</td>
@@ -97,71 +96,70 @@ class GF_Custom_JS {
 				rgar( $form, 'customJS' )
 			),
 		);
-
+		
 		return $settings;
 	}
-
+	
 	public function save_custom_js_setting( $form ) {
 		$form['customJS'] = esc_html( rgpost( 'custom_js' ) );
+		
 		return $form;
 	}
-
+	
 	public function load_form_script( $form, $is_ajax_enabled ) {
-
-		if( $this->is_applicable_form( $form ) && ! has_action( 'wp_footer', array( $this, 'output_script' ) ) ) {
+		if ( $this->is_applicable_form( $form ) && ! has_action( 'wp_footer', array( $this, 'output_script' ) )
+		) {
 			$this->queue_script( $form['id'], $this->get_custom_js( $form ) );
-			add_action( 'wp_footer', array( $this, 'output_script' ), 99 );
+			add_action( 'wp_footer',            array( $this, 'output_script' ), 99 );
 			add_action( 'gform_preview_footer', array( $this, 'output_script' ), 99 );
 		}
-
+		
 		return $form;
 	}
-
+	
 	public function queue_script( $form_id, $script ) {
 		$this->scripts[ $form_id ] = $script;
 	}
-
+	
 	public function get_script_queue() {
 		return $this->scripts;
 	}
-
+	
 	public function output_script() {
-
 		$allowed_entities = array(
 			'&#039;' => '\'',
 			'&quot;' => '"',
 		);
-
+		
 		?>
-
-		<script type="text/javascript">
-
-			( function( $ ) {
-
-				$( document ).bind( 'gform_post_render', function() {
-
-					<?php foreach( $this->get_script_queue() as $script ):
-						echo html_entity_decode( str_replace( array_keys( $allowed_entities ), array_values($allowed_entities), $script ) );
-					endforeach; ?>
-
-				} );
-
-			} )( jQuery );
-
-		</script>
-
+<script type='text/javascript'>
+(function ($) {
+	$(document).bind('gform_post_render', function () {
+		<?php foreach ( $this->get_script_queue() as $script ):
+			echo str_replace("\n", "\n\t\t",
+				html_entity_decode(
+					str_replace( array_keys( $allowed_entities ), array_values( $allowed_entities ), $script )
+				)
+			);
+		endforeach; ?>
+	
+	});
+})(jQuery);
+</script>
+		
 		<?php
 	}
-
+	
 	public function get_custom_js( $form ) {
 		return rgar( $form, 'customJS' );
 	}
-
+	
 	public function is_applicable_form( $form ) {
 		$js = $this->get_custom_js( $form );
+		
 		return ! empty( $js );
 	}
-
+	
 }
 
 function gw_custom_js() {
